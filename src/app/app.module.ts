c31fd8a455cbdc7e2dc314c323676bbb83650137
jecofa <jecofa@gmail.com>
2018-11-23 14:29:27 -0500

    first site web

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IntroComponent } from './intro/intro.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { TestimonioComponent } from './testimonio/testimonio.component';
+import { ClientComponent } from './client/client.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { FooterComponent } from './footer/footer.component';
+import { SectionComponent } from './section/section.component';
+import { HeaderComponent } from './header/header.component';
+import { SocialComponent } from './social/social.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroComponent,
+    GalleryComponent,
+    TestimonioComponent,
+    ClientComponent,
+    PricingComponent,
+    FooterComponent,
+    SectionComponent,
+    HeaderComponent,
+    SocialComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/testimonio/testimonio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-testimonio',
+  templateUrl: './testimonio.component.html',
+  styleUrls: ['./testimonio.component.css']
+})
+export class TestimonioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
